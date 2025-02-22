56ca3ed70eaebeef6b04aa5bccb2e48d79e493cc
Foysal Ahmmed Limon <info@easytrax.com.bd>
2025-02-22 23:54:36 +0600

    added sell item logic

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegistrationForm, LoginForm, PurchaseItemForm
+from market.forms import RegistrationForm, LoginForm, PurchaseItemForm, SellItemForm
 from flask_login import login_user, logout_user, login_required, current_user
 
 
@@ -15,8 +15,10 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    sell_form = SellItemForm()
 
     if request.method == "POST":
+        # Purchase item logic
         purchased_item = request.form.get("purchased_item")
         p_item_object = Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
@@ -32,11 +34,34 @@ def market_page():
                     category="danger",
                 )
 
-            return redirect(url_for("market_page"))
+        # Sell item logic
+        sold_item = request.form.get("sold_item")
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(
+                    f"Congratulations! You sold {s_item_object.name} back to market!",
+                    category="success",
+                )
+            else:
+                flash(
+                    f"Something went wrong with selling {s_item_object.name}",
+                    category="danger",
+                )
+
+        return redirect(url_for("market_page"))
 
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
-        return render_template("market.html", items=items, purchase_form=purchase_form)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template(
+            "market.html",
+            items=items,
+            purchase_form=purchase_form,
+            sell_form=sell_form,
+            owned_items=owned_items,
+        )
 
 
 @app.route("/register", methods=["GET", "POST"])
